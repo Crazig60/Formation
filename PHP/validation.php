994f618ff8c36a03f2119fce500b5b97381f60f2
Crazig60 <jerome.caouren@gmail.com>
2019-11-21 23:59:26 +0400

    modification code php

--- PHP/validation.php
+++ PHP/validation.php
@@ -8,13 +8,13 @@ $head .= "Return-Path: <".$adresse.">\n";
 $head .= "Content-Type: text/plain; charset=iso-8859-1\n";
 $sujet = "Formulaire formation DREAMWEAVER CC";
 $informations = "
-Civilité: ".$_POST['Civilité']." \r\n
+Civilité: ".$_POST['Civilite']." \r\n
 Nom: ".$_POST['Nom']." \r\n
-Prénom: ".$_POST['Prénom']." \r\n
+Prénom: ".$_POST['Prenom']." \r\n
 Sport: ".$_POST['sport']." \r\n
-Sport: ".$_POST['niveau-sport']."\r\n
+Niveau: ".$_POST['niveau-sport']."\r\n
 Email du formulaire: ".$_POST['email']." \r\n
-Message: ".$_POST['message']." \r\n
+Message: ".$_POST['Message']." \r\n
 ";
 $res = mail($TO ,$sujet ,$informations, $head);
 
